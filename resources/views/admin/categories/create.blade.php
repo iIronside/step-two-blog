7fc5488f13b23fb55033bf5144f6610387c9848f
iironside <iironside@yandex.com>
2022-02-17 16:40:28 +0300

    add category create

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -3,10 +3,14 @@
 @section('content')
     <h4>Создание категории</h4>
 
-    <form action="#" class="w-25">
+    <form action="{{route('admin.categories.store')}}" method="POST" class="w-25">
+        @csrf
         <div class="form-group">
             <label>Название</label>
-            <input type="password" class="form-control" placeholder="Название категории">
+            <input type="text" name="title" class="form-control" placeholder="Название категории">
+            @error('title')
+                <div class="text-danger">{{$message}}</div>
+            @enderror
         </div>
         <input type="submit" class="btn btn-primary" value="Добавить">
     </form>
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -2,7 +2,39 @@
 
 @section('content')
     <h4>Категории</h4>
-    <div class="col-1">
+    <div class="col-1 mb-3">
         <a href="{{route('admin.categories.create')}}" type="button" class="btn btn-block btn-primary">Создать</a>
     </div>
+
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Categories Table</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-hover text-nowrap">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($categories as $category)
+                            <tr>
+                                <td>{{$category->id}}</td>
+                                <td>{{$category->title}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+    </div>
+
 @endsection
